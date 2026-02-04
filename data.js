fd7552d7bcf72d64ad762ca3231df8dc45c6f6dc
Chenz-cloud <geydira@gmail.com>
2026-02-04 12:57:38 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const motifList = [
+  "Batik Mega Mendung",
+  "Batik Parang",
+  "Batik Kawung",
+  "Batik Truntum",
+  "Batik Sekar Jagad",
+  "Batik Lasem",
+  "Batik Gentongan",
+  "Batik Cuwiri",
+  "Batik Sidomukti",
+  "Batik Sidoluhur"
+];
