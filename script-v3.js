28a0121b57e6ed38f1f9948b651a4d83604a76b4
Chenz-cloud <geydira@gmail.com>
2026-02-04 17:31:18 +0700

    Update script-v3.js

--- script-v3.js
+++ script-v3.js
@@ -79,3 +79,18 @@ pdfPreview.onclick = function (e) {
     pdfPreview.classList.add("hidden");
   }
 };
+function loadImageToBase64(url) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      resolve(canvas.toDataURL("image/jpeg"));
+    };
+    img.src = url;
+  });
+}
