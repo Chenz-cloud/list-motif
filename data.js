5c551a3bb95e9429953c1e7182976e6cc37073e1
Chenz-cloud <geydira@gmail.com>
2026-02-04 18:33:09 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,17 @@
-const FOTO_FOLDER = "photos/25x50/";
-
 const motifList = [
-  "Aurora blue",
-  "Aurora bg",
-  "Anaheim gn",
-  "Autumn bl",
-  "Camrose gy",
-  "Camrose wt",
-  "Interlaken tc",
-  "Livia wt",
-  "Maldives pk",
-  "Maxine bl",
-  "Padova cm",
-  "Rhein bk",
-  "Seine wt",
-  "Waikiki bg",
-  "Waikiki gy"
-].map(nama => ({
-  nama: nama,
-  foto: FOTO_FOLDER + nama.toLowerCase().replace(/ /g, "-") + ".jpg"
-}));
+  "aurora blue",
+  "aurora bg",
+  "anaheim gn",
+  "autumn bl",
+  "camrose gy",
+  "camrose wt",
+  "interlaken tc",
+  "livia wt",
+  "maldives pk",
+  "maxine bl",
+  "padova cm",
+  "rhein bk",
+  "seine wt",
+  "waikiki bg",
+  "waikiki gy"
+];
