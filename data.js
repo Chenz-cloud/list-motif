18d6f60e9aff54590977f47b73fe65dd0e335f91
Chenz-cloud <geydira@gmail.com>
2026-03-06 13:19:20 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 const motifList = [
   { nama: "Aurora blue", foto: "photos/25x50/aurora-blue.jpg" },
-  { nama: "Aurora bg", foto: "photos/25x50/aurora-bg.jpg" }, // Pastikan nama filenya aurora-bg.jpg
+  { nama: "Aurora bg", foto: "photos/25x50/aurora-bg.jpg" },
   { nama: "Anaheim gn", foto: "photos/25x50/anaheim-gn.jpg" },
-  { nama: "Autumn blue", foto: "photos/25x50/autumn-blue.jpg" }, // Sebelumnya mungkin salah ketik 'autumb'
+  { nama: "Autumn blue", foto: "photos/25x50/autumn-blue.jpg" },
   { nama: "Camrose grey", foto: "photos/25x50/camrose-grey.jpg" },
   { nama: "Camrose wt", foto: "photos/25x50/camrose-wt.jpg" },
   { nama: "Interlaken tc", foto: "photos/25x50/interlaken-tc.jpg" },
@@ -13,12 +13,10 @@ const motifList = [
   { nama: "Rhein bk", foto: "photos/25x50/rhein-bk.jpg" },
   { nama: "Seine wt", foto: "photos/25x50/seine-wt.jpg" },
   { nama: "Waikiki bg", foto: "photos/25x50/waikiki-bg.jpg" },
-  { nama: "Waikiki gy", foto: "photos/25x50/waikiki-gy.jpg" }
-  
+  { nama: "Waikiki gy", foto: "photos/25x50/waikiki-gy.jpg" }, // <-- Pastikan ada koma di sini
   { nama: "Zenica bg", foto: "photos/25/40/zenica-bg.jpg" },
   { nama: "Capri gy", foto: "photos/25/40/capri-gy.jpg" },
   { nama: "Capri bg", foto: "photos/25/40/capri-bg.jpg" },
   { nama: "Marlin gy", foto: "photos/25/40/marlin-gy.jpg" },
   { nama: "Sacramento bn", foto: "photos/25/40/sacramento-bn.jpg" }
-
 ];
